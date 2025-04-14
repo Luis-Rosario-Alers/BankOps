5bc3d7dac25e817f0434e3bd6d280b1b5568859e
Luis Rosario Alers <luisrosarioalers@gmail.com>
2025-04-13 22:54:34 -0500

    :construction: add custom widget for summary cards with WIP animations

--- src/ui/plugins/widgets/summary_card_widget.py
+++ src/ui/plugins/widgets/summary_card_widget.py
@@ -1,10 +1,35 @@
-from PySide6.QtGui import QFont
+import PySide6.QtCore
+from PySide6.QtCore import (
+    QEasingCurve,
+    QEvent,
+    QPoint,
+    QPropertyAnimation,
+    QSequentialAnimationGroup,
+)
+from PySide6.QtGui import QFont, QPainter
 from PySide6.QtWidgets import QFrame, QHBoxLayout, QLabel, QVBoxLayout
 
 
 class SummaryCardWidget(QFrame):
     def __init__(self, title, value, is_positive=True, parent=None):
+        self._is_hovered = False
         super().__init__(parent)
+
+        self._raised_position = QPoint(0, 0)
+        self.move_animation = QPropertyAnimation(self, b"raised_position", self)
+        self.move_animation.setEasingCurve(QEasingCurve.OutElastic)
+        self.move_animation.setDuration(400)
+
+        # I haven't created the property for this animation yet.
+        # self.animation2 = QPropertyAnimation(self, b"lowered_position", self)
+        # self.animation2.setEasingCurve(QEasingCurve.InBounce)
+        # self.animation2.setDuration(100)
+        # self.animation2.setEndValue(1000)
+
+        self.animations_group = QSequentialAnimationGroup(self)
+        self.animations_group.addAnimation(self.move_animation)
+        # self.animations_group.addAnimation(self.animation2)
+
         self.setObjectName("summaryCard")
 
         # Set frame styling
@@ -45,3 +70,41 @@ class SummaryCardWidget(QFrame):
         main_layout.addWidget(self.title_label)
         main_layout.addLayout(value_layout)
         main_layout.addStretch()
+
+    def start_animation(self):
+        self.animations_group.stop()
+        # FIXME: figure out why this bugs out on window resizes.
+        if self._is_hovered:
+            self.move_animation.setEndValue(QPoint(self.x(), -10))
+        else:
+            self.move_animation.setEndValue(QPoint(self.x(), 0))
+        self.update()
+        self.animations_group.start()
+
+    def paintEvent(self, event) -> None:
+        painter = QPainter(self)
+        print("yadiel")
+        print(self._raised_position)
+
+        painter.setPen("blue")
+        painter.drawRect(self.contentsRect())
+        painter.end()
+
+    def enterEvent(self, event: QEvent):
+        self._is_hovered = True
+        self.start_animation()
+
+    def leaveEvent(self, event: QEvent):
+        self._is_hovered = False
+        self.start_animation()
+
+    @PySide6.QtCore.Property(QPoint)
+    def raised_position(self):
+        return self._raised_position
+
+    @raised_position.setter
+    def raised_position(self, pos: QPoint):
+        if pos != self._raised_position:
+            self._raised_position = pos
+            self.move(pos)
+            self.update()
